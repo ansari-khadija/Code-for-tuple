ca25116e9f6560bf55e4132d08a7064ac928b766
Khadija Ansari <ansarikubra2006@gmail.com>
2026-09-02 10:15:42 +0530

    Create 12_Typecasting.py

---
+++ 12_Typecasting.py
@@ -0,0 +1,24 @@
+#Type casting for list
+
+#string to list
+a="python"
+print(list(a))
+       #OR
+b=input("enter a word:")
+print(list(b))
+
+#tuple to list
+c=("python","JAVA","C++","HTML")
+print(list(c))
+
+#set to list
+d={"blue","pink","red","black"}
+print(list(d))
+
+#list to tuple
+c=["python","JAVA","C++","HTML"]
+print(tuple(c))
+
+#set to list
+d=["blue","pink","red","black"]
+print(set(d))

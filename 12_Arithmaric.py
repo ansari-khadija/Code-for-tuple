0e4abe4930d58cb43ab8504d84eb0dcb031adc66
Khadija Ansari <ansarikubra2006@gmail.com>
2026-09-06 10:50:59 +0530

    Add basic arithmetic operations using a tuple

---
+++ 12_Arithmaric.py
@@ -0,0 +1,13 @@
+# Create a tuple
+numbers = (10, 20, 30, 40)
+
+# Arithmetic operations
+addition = numbers[0] + numbers[1]
+subtraction = numbers[2] - numbers[0]
+multiplication = numbers[1] * numbers[3]
+division = numbers[3] / numbers[0]
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)

af948d7409bff19c06d7861ed5f9cdeb9a4b6c81
Khadija Ansari <ansarikubra2006@gmail.com>
2026-09-06 10:57:14 +0530

    Enhance 13_Operations.py with additional operations
    Added membership, comparison, and identity operations to the arithmetic example.

--- 13_Operations.py
+++ 13_Operations.py
@@ -1,5 +1,5 @@
 # Create a tuple
-numbers = (10, 20, 30, 40)
+numbers = (10, 20, 30, 40, 50)
 
 # Arithmetic operations
 addition = numbers[0] + numbers[1]
@@ -11,3 +11,28 @@ print("Addition:", addition)
 print("Subtraction:", subtraction)
 print("Multiplication:", multiplication)
 print("Division:", division)
+
+# Membership operators
+print(20 in numbers)
+print(60 in numbers)
+
+print(20 not in numbers)
+print(60 not in numbers)
+
+#Comparison
+a = (1, 2, 3)
+b = (1, 2, 4)
+
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+
+#Identity
+a = (1, 2, 3)
+b = a
+
+print(a is b)
+print(a is not b)
+
+

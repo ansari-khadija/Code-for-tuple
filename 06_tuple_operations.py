d6684ec8f5497fd983b0c90c629cf0ecabbcf2a7
Khadija Ansari <ansarikubra2006@gmail.com>
2026-09-09 10:33:45 +0530

    Update 06_tuple_operations.py

--- 06_tuple_operations.py
+++ 06_tuple_operations.py
@@ -1,37 +1,42 @@
 # Tuple Operations
 
-tuple1 = ("Python", "Java", "C++")
-tuple2 = ("SQL", "HTML", "CSS")
+tuple1 = tuple(map(int, input("Enter numbers for Tuple 1: ").split()))
+tuple2 = tuple(map(int, input("Enter numbers for Tuple 2: ").split()))
+
 
 print("Tuple 1:", tuple1)
 print("Tuple 2:", tuple2)
 
-# 1. Concatenation
+# 1. Concatenation/arithmetic-addition
 print("\n--- Concatenation ---")
 
 combined = tuple1 + tuple2
 print("Combined tuple:", combined)
 
 
-# 2. Repetition
+# 2. Repetition/arithmetic-multiplication
 print("\n--- Repetition ---")
 
-repeated = ("Python", "Java") * 2
-print("Repeated tuple:", repeated)
+repeated = tuple1 * 2
+print("Repeated tuple1:", repeated)
+repeated = tuple2 * 2
+print("Repeated tuple2:", repeated)
+repeated = combined * 2
+print("Repeated combine tuple:", repeated)
 
 
 # 3. Membership operator
 print("\n--- Membership ---")
 
-print("Python" in tuple1)
-print("SQL" in tuple1)
+print(tuple1 in tuple2)
+print(tuple2 in tuple1)
 
 
 # 4. Not in operator
 print("\n--- Not in ---")
 
-print("HTML" not in tuple1)
-print("Java" not in tuple1)
+print(tuple1 not in combined)
+print(tuple2[0] not in combined)
 
 
 # 5. Length of tuple

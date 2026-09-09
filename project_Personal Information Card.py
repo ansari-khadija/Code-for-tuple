79300f8dc9eee86c133c13b1c7dff51470be6721
Khadija Ansari <ansarikubra2006@gmail.com>
2026-09-09 10:59:35 +0530

    Create project_Personal Information Card.py

---
+++ project_Personal Information Card.py
@@ -0,0 +1,16 @@
+person = ("Rahul", 20, "Pune", "Python")
+
+print("Name:", person[0])
+print("Age:", person[1])
+print("City:", person[2])
+print("Course:", person[3])
+
+# Description
+'''
+Goal: Practice tuple creation, indexing, and simple output.
+
+I learn and practice:
+1. Tuple creation
+2. Indexing
+3. print()
+'''
